ea543b51600b17570d532994eac1941b7207531b
grish2ry <grish2ry@gmail.com>
2026-05-02 18:00:48 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ class Program
 {
     static async Task Main(string[] args)
     {
+        \\Пример работы (написано ИИ)
         var taskService = new TaskService();
         
         // Сценарий 1: Загрузка с обработкой ошибок
